d14330a555aade3fbfea40ea2c76f4fe399c13df
sreeragmsurendran <sreeragmsurendran1997@gmail.com>
2025-01-13 21:51:24 +0530

    Order and sigin in and login

--- nextecom/components/shared/header/index.tsx
+++ nextecom/components/shared/header/index.tsx
@@ -1,5 +1,5 @@
-import { APP_NAME } from '@/lib/constants'
-import Image from 'next/image'
+// import { APP_NAME } from '@/lib/constants'
+// import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import Search from './search'
